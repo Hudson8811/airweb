94c7b692ba979e46dacd95c5acd6311c684832c8
Hudson8811 <hudson88@yandex.ru>
2020-05-26 14:19:45 +0400

    mobile menu

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
 	$('.header__hamburger').click(function () {
-	})
+		$('.header-mobile-nav').addClass('header-mobile-nav--move');
+		$('body').addClass('body-scroll-lock');
+	});
+
+	$('.header-mobile-nav__close').click(function () {
+		$('.header-mobile-nav').removeClass('header-mobile-nav--move');
+		$('body').removeClass('body-scroll-lock');
+	});
+
+	$('.header-mobile-nav__show-btn').each(function () {
+		$(this).click(function () {
+			$(this).children('.header-mobile-nav__show-btn-wrapper').children('.header-mobile-nav__arrow').toggleClass('header-mobile-nav__arrow--rotate');
+			$(this).next().slideToggle(300);
+		});
+	});
 });
