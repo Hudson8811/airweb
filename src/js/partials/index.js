057b38742c9b4e7b45656fbf5e05690cd27e0832
Hudson8811 <hudson88@yandex.ru>
2020-05-29 14:02:10 +0400

    cases block

--- src/js/partials/index.js
+++ src/js/partials/index.js
@@ -10,5 +10,25 @@ $(document).ready(function () {
 		},
 		infinite: true
 	});
+
+	$('.case__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<button class="case__arrow case__arrow-prev" aria-hidden="true"><span></span></button>',
+		nextArrow: '<button class="case__arrow case__arrow-next" aria-hidden="true"><span></span></button>',
+		dots: true,
+		customPaging: function() {
+			return '<button class="case__dot"></button>';
+		},
+		infinite: true,
+		responsive: [
+			{
+				breakpoint: 1000,
+				settings: {
+					dots: false
+				}
+			}
+		]
+	});
 });
 
